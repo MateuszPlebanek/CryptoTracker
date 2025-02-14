93ceef9ab1b5e89e7f784c58bdf49708d6b3c31f
MateuszPlebanek <matprocom@gmail.com>
2025-02-14 12:25:20 +0100

    Finalisation de l'US03 barre de recherche + début page login

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import HexagonBubblesContainer from "../components/HexagonBubblesContainer";
+
+const HomePage = () => {
+	return (
+		<div className="home-page">
+			<HexagonBubblesContainer />
+		</div>
+	);
+};
+
+export default HomePage;
