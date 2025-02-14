6506e6bd03e08b52840ce2617297c1b6458607f9
MateuszPlebanek <matprocom@gmail.com>
2025-02-14 07:52:57 +0100

    [FEAT] Header avec nom et icône utilisateur

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css"; // Ajout de Bootstrap
 import "./index.css";
 import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
 
 const rootElement = document.getElementById("root");
 if (rootElement) {
 	createRoot(rootElement).render(
 		<StrictMode>
+			<BrowserRouter>
 			<App />
+			</BrowserRouter>
 		</StrictMode>,
 	);
 } else {
